0343c1d3c5eef3d5d0bdf62f1bab8ff547aaafe4
DeerByte <55000332+DeerByte@users.noreply.github.com>
2020-11-01 16:02:39 -0700

    Moved Node class to Node.java
    @justingajer

---
+++ Node.java
@@ -0,0 +1,46 @@
+/**
+ * @author Justin 
+ * TODO: finish Javadoc
+ */
+public class Node<T extends Comparable<T>> {
+
+    private T data;
+    private Node<T> next;
+
+    public Node() {
+        data = null;
+        next = null;
+    }
+    /**
+     * Constructor for objects of class Node
+     */
+    public Node(T data) {
+        this.data = data;
+        next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T o) {
+        data = o;
+        }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> n) {
+        next = n;
+    }
+
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    public String toString() {
+        return "Node: " + getData().toString();
+    }
+
+}
