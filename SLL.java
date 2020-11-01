e9bef48d2a2c42d71de3baf25c7f0a659fddf892
justingajer <60195553+justingajer@users.noreply.github.com>
2020-10-31 19:33:58 -0600

    Add files via upload
    I've changed some of the code to take in avenger objects to add to the node and added a local compare method within the SLL class

--- SLL.java
+++ SLL.java
@@ -1,23 +1,42 @@
+import java.util.Comparator;
+
 public class SLL<T extends Comparable<T>> {
 
-	public SLL<T> firstLink;
-	private SLL<T> next;
+	private Node<T> head;
+	private int size;
+	private Node<T> next;
+	
+	
+	//I haven't created this class since someone else was doing that.
+	private Comparator<T> comparator;
 	
 	//To add avengers to list, don't know if we need this though
-	private String Name;
-	private String Alias;
-	private int Freq;
 
 
 	//A little confused do I put our avengers variables in our list?
-	public SLL(String N, String A, int F) {
-		Name = N;
-		Alias = A;
-		Freq = F;
+	public SLL() {
+		head = null;
+		size = 0;
+		comparator = null;
 	}
 
+	
+	public SLL(Comparator<T> externalComp) {
+		head = null;
+		size = 0;
+		comparator = externalComp;
+	}
+	
+	//checking 2 objects
+	private int compare(T o1, T o2) {
+		if (comparator == null)
+			return o1.compareTo(o2);
+		else
+			return comparator.compare(o1,o2);
+	}
+	
 	public boolean isEmpty() {
-		return (firstLink == null);
+		return (head == null);
 	}
 
 	//Creating Node Class
@@ -25,12 +44,13 @@ public class SLL<T extends Comparable<T>> {
 
 		private T data;
 		public Node<T> next;
+		public String Name;
 
 		/**
 		 * Constructor for objects of class Node
 		 */
-		public Node(T d) {
-			data = d;
+		public Node(T avenger) {
+			data = avenger;
 			next = null;
 		}
 
@@ -40,7 +60,7 @@ public class SLL<T extends Comparable<T>> {
 
 		public void setData(T o) {
 			data = o;
-		}
+			}
 
 		public Node<T> getNext() {
 			return next;
@@ -65,10 +85,10 @@ public class SLL<T extends Comparable<T>> {
 	 * 
 	 * 
 	 */
-	public void insertFirstLink(String Name, String Alias, int Frequency) {
-		SLL<T> newLink = new SLL<T>(Name, Alias, Frequency);
-		newLink.next = firstLink;
-		firstLink = newLink;
+	public void insertFirstLink(T Avenger) {
+		Node<T> newLink = new Node<T>(Avenger);
+		newLink.next = head;
+		head = newLink;
 	}
 
 	/*
@@ -79,10 +99,10 @@ public class SLL<T extends Comparable<T>> {
 	 * 
 	 * 
 	 */
-	public SLL<T> removeLink(String Name) {
+	public Node<T> removeLink(String Name) {
 
-		SLL<T> currentLink = firstLink;
-		SLL<T> previousLink = firstLink;
+		Node<T> currentLink = head;
+		Node<T> previousLink = head;
 
 		while (currentLink.Name != Name) {
 			if (currentLink.next == null) {
@@ -92,18 +112,24 @@ public class SLL<T extends Comparable<T>> {
 				currentLink = currentLink.next;
 			}
 		}
-		if (currentLink == firstLink) {
-			firstLink = firstLink.next;
+		if (currentLink == head) {
+			head = head.next;
 		} else {
 
 			System.out.println("match found");
 			System.out.println("currentLink: " + currentLink);
-			System.out.println("firstLink: " + firstLink);
+			System.out.println("firstLink: " + head);
 
 			previousLink.next = currentLink.next;
 		}
 		return currentLink;
 	}
+	
+	//returns size
+	public int size() {
+		return size;
+	}
+	
 	
 	/*
 	 * Find Method
@@ -113,9 +139,9 @@ public class SLL<T extends Comparable<T>> {
 	 * 
 	 * 
 	 */
-	public SLL<T> find(String name){	
-		SLL<T> theLink = firstLink;	
-		
+	
+	public Node<T> find(String name){	
+		Node<T> theLink = head;			
 		if(!isEmpty()){
 			while(theLink.Name != name){
 				if(theLink.next == null){
@@ -138,4 +164,5 @@ public class SLL<T extends Comparable<T>> {
 	 * 
 	 * 
 	 */
+	
 }
