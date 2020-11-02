fec0df20eb385552f7d5ae118189fb74070d68b7
Zain <73448476+Zain4271@users.noreply.github.com>
2020-11-01 18:07:26 -0700

    Comparator

---
+++ AscendingComparator.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+public class AscendingComparator implements Comparator<Avenger>{
+
+    public int compare(Avenger a1, Avenger a2){
+        int compFreq= a1.getFreqMentioned() - a2.getFreqMentioned();
+
+
+        if (compFreq==0){
+            return a2.getAlias().compareTo(a1.getAlias())
+
+
+        }
+        else{
+            return compFreq;
+        }
+
+
+        }
+    }
---
+++ DescendingComparator.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public class DescendingComparator implements Comparator<Avenger> {
+
+	public int compare(Avenger a1, Avenger a2) {
+		int compFrequency = a2.getFreqMentioned() - a1.getFreqMentioned();
+		
+		if (compFrequency == 0) {
+			return a1.getAlias().compareTo(a2.getAlias());
+			
+		} else {
+            return compFrequency;
+		}
+	}
+}
